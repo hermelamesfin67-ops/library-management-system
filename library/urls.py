d769acad91b116cb5aaed09b993fee5f386adf48
hermelamesfin67-ops <hermelamesfin67@gmail.com>
2026-07-18 14:13:29 +0300

    add health check

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import BookList, BookDetails
+from django.http import JsonResponse
 
 urlpatterns = [
   # path('check/', views.Check_available , name='Check_available'),
   path("api/books/",BookList.as_view() ,name="all_books"),
   path("api/books/<int:pk>/",BookDetails.as_view() ,name="book_details"),
+  path("health-check/", lambda request: JsonResponse({"status": "ok"}), name="health-check"),
 ]
